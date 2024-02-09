76b3f89ce74d6157b15bd7b7f66f385fe591bd51
SIANG-eric <eex790619@gmail.com>
2024-02-09 08:54:48 +0800

    create about amber Login

---
+++ src/views/Login_in.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="about_Lo_main">
+        <form action="" class="about_Lo_container">
+            <h1 class="about_Lo_h1">會員登入</h1>
+
+            <!---------------------------輸入選項-------------------------->
+            
+            <h3 class="about_Lo_h3">帳號</h3>
+            <input type="text" class="about_Lo_input" placeholder="請輸入Email">
+
+            <h3 class="about_Lo_h3 Lo_h3_1">密碼</h3>
+            <input type="password" class="about_Lo_input  " id="password" placeholder="請輸入密碼" >
+            <i class="fa-solid fa-eye password"  id="eye" ></i>
+
+            <!---------------------------連結-------------------------->
+
+            <div class="about_Lo_a">
+                <a href="#">立即註冊</a>
+                <a href="#">忘記密碼?</a>
+            </div>
+
+            <!---------------------------按鈕-------------------------->
+            <button class="about_button">登入</button>
+            <h5 class="about_h5">或其他方式註冊</h5>
+            <!---------------------------google-------------------------->
+            <a class="about_button2" href="#">
+                <img src="../assets/image/about/google.png" alt="">
+                <h3>以Google帳號註冊</h3>
+            </a>
+        </form>    
+        
+        
+    </div>
+</template>
+<style lang="scss">
+  @import"../assets/sass/style.scss";
+</style>
---
+++ src/views/member.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="member_container">
+
+        <form action="" class="member_form">
+
+            <h1>註冊</h1>
+            
+        <!---------------------------輸入選項-------------------------->  
+            
+            <h3>姓名</h3>
+            <input type="text" placeholder="請輸入姓名">
+            <h3>信箱</h3>
+            <input type="text" placeholder="請輸入Email">
+            <h3>密碼</h3>
+            <input type="text" placeholder="請輸入密碼" >
+            <h3>確認密碼</h3>
+            <input type="text" placeholder="請再輸入一次密碼">
+        
+        <!---------------------------按鈕-------------------------->
+        <button>立即加入</button>
+        <h5 >或其他方式註冊</h5>
+        <a class="about_button2" href="#">
+            <img src="../assets/image/about/google.png" alt="">
+            <h3 class="google">以Google帳號註冊</h3>
+        </a>
+            
+        </form>
+</div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss">
+  @import"../assets/sass/style.scss";
+</style>
