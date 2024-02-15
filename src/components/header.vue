094c4a52a20116e11b1b246c41e84a737b9a37d6
XinLiao88 <ninexin8888@gmail.com>
2024-02-15 13:37:09 +0800

    首頁banner+首尾RWD

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,21 +1,22 @@
 <template>
-      <header>
-        <nav>
-            <RouterLink to="./Homeview"><img src="../assets/image/pic/icon/logo.svg" alt="" class="logo"></RouterLink>
-            <ul class="empty"></ul>
-            <ul class="menu">
-                <li><RouterLink to="./about">關於我們</RouterLink></li>
-                <li><RouterLink to="./info">最新消息</RouterLink></li>
-                <!-- <RouterLink to="/about">about</RouterLink> -->
-                <li><RouterLink to="/products">飲品菜單</RouterLink></li>
-                <li><RouterLink to="#">門市據點</RouterLink></li>
-            </ul>
-        <ul class="member_btn">
-            <li><RouterLink to="#"><i class="fa-solid fa-user"></i></RouterLink></li>
-            <li><a href="#" @click.prevent><i class="fa-solid fa-cart-shopping" @click="toggleSlide"></i></a></li>
-        </ul>
+  <header>
+    <nav>
+      <i class="fa-solid fa-bars"></i>
+      <RouterLink to="./Homeview"><img class="logo" src="../assets/image/pic/icon/logo.svg" alt=""></RouterLink>
+      <ul class="empty"></ul>
+      <ul class="menu">
+          <li><RouterLink to="./about">關於我們</RouterLink></li>
+          <li><RouterLink to="./info">最新消息</RouterLink></li>
+          <!-- <RouterLink to="/about">about</RouterLink> -->
+          <li><RouterLink to="/products">飲品菜單</RouterLink></li>
+          <li><RouterLink to="#">門市據點</RouterLink></li>
+      </ul>
+      <ul class="member_btn">
+          <li><RouterLink to="./Login_in"><i class="fa-solid fa-user"></i></RouterLink></li>
+          <li><a href="#" @click.prevent><i class="fa-solid fa-cart-shopping" @click="toggleSlide"></i></a></li>
+      </ul>
     </nav>
-</header>
+  </header>
 
 </template>
 <script>
