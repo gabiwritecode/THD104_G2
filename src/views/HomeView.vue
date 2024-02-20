15968e9f755ce775f7f5d154f5be36c704caa118
XinLiao88 <ninexin8888@gmail.com>
2024-02-21 01:08:21 +0800

    首頁top5照片 select背景圖 範例檔html @Nora

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@ export default {
   //   const headerBgc = document.querySelector('.page-home').querySelector('header')
   //   // console.log(headerBgc);
   //   window.addEventListener('scroll', function () {
-  //   if (window.scrollY > 250) {
+  //   if (window.scrollY > 120) {
   //     headerBgc.classList.add('header_bgc');
   //   } else {
   //     headerBgc.classList.remove('header_bgc');
@@ -20,7 +20,7 @@ export default {
     const headerBgc = ref(null);
 
     const handleScroll = () => {
-      if (window.scrollY > 250) {
+      if (window.scrollY > 120) {
         headerBgc.value.classList.add('header_bgc');
       } else {
         headerBgc.value.classList.remove('header_bgc');
@@ -58,7 +58,8 @@ export default {
       
       <div class="container">
         <!-- <div class="beverage_container"> -->
-          <h1>BOISSON</h1>
+          <!-- <h1>BOISSON</h1> -->
+          <img class="boisson_gradient" src="../assets/image/index/boisson_gradient.png" alt="">
           <p>捌嵩飲茶，品味悠久的文化底蘊，感受時光與茶的對話。</p>
           <img class="ball1" src="../assets/image/index/ball1.png" alt="">
           <img class="ball2" src="../assets/image/index/ball2.png" alt="">
@@ -120,13 +121,13 @@ export default {
         <h1>TOP 5 熱銷飲品</h1>
         <ul>
           <li class="top_products product1">
-            <img src="../assets/image/products/naixi_qubei.png" alt="">
+            <img src="../assets/image/index/top5_beverage1.png" alt="">
             <h2>愛爾蘭巧克力奶昔</h2>
             <p>享受絲滑口感與甜蜜幸福的奢華體驗。</p>
           </li>
 
           <li class="top_products product2">
-            <img src="../assets/image/products/naixi_qubei.png" alt="">
+            <img src="../assets/image/index/top5_beverage2.png" alt="">
             <h2>愛爾蘭巧克力奶昔</h2>
             <p>享受絲滑口感與甜蜜幸福的奢華體驗。</p>
           </li>
@@ -138,12 +139,12 @@ export default {
           </li>
 
           <li class="top_products product4">
-            <img src="../assets/image/products/naixi_qubei.png" alt="">
+            <img src="../assets/image/index/top5_beverage4.png" alt="">
             <h2>愛爾蘭巧克力奶昔</h2>
             <p>享受絲滑口感與甜蜜幸福的奢華體驗。</p>
           </li>
           <li class="top_products product5">
-            <img src="../assets/image/products/naixi_qubei.png" alt="">
+            <img src="../assets/image/index/top5_beverage5.png" alt="">
             <h2>愛爾蘭巧克力奶昔</h2>
             <p>享受絲滑口感與甜蜜幸福的奢華體驗。</p>
           </li>
