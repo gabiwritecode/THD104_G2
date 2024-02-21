3d9670668dc9f0fad55a7e5bc697818e4662a489
XinLiao88 <ninexin8888@gmail.com>
2024-02-21 20:23:26 +0800

    後台商品管理 後台登入 漢堡修改 @Nora

---
+++ src/views/backend/BackEndLogin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="space"></div>
+    <div class="backend_login">
+        <img src="../../assets/image/pic/icon/logo.svg" alt="">
+        <p>捌嵩飲茶後台管理</p>
+
+        <h2>登入後台</h2>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
