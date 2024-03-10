cd14a3ecec9e952f9f033d7dd38a21c983f98e05
XinLiao88 <ninexin8888@gmail.com>
2024-03-11 02:34:17 +0800

    入口rwd+wave、修正購物車radio手機版位移 @Nora

--- src/views/Entrance.vue
+++ src/views/Entrance.vue
@@ -1,21 +1,39 @@
 <template>
-    <div class="space"></div>
     <main class="entrance_wrapper">
-        <ul>
-            <li class="btn">
-                <RouterLink to="./Homeview">前台
+        <div class="container">
+            <ul>
+                <li class="btn">
+                    <RouterLink to="./Homeview">
                     <img class="cup" src="../assets/image/Entrance/cup.svg" alt="">
-                </RouterLink>
-            </li>
-            <li><img class="logo" src="../assets/image/Entrance/logo_Entrance.svg" alt=""></li>
-            <li class="btn">
-                <RouterLink to="./backendLogin">後台
+                    <span>前台</span>
+                    </RouterLink>
+                </li>
+                <li><img class="logo" src="../assets/image/Entrance/logo_Entrance.svg" alt=""></li>
+                <li class="btn">
+                    <RouterLink to="./backendLogin">
                     <img class="cup" src="../assets/image/Entrance/cup.svg" alt="">
+                    <span>    後台</span>
                 </RouterLink>
-            </li>
-        </ul>
+                </li>
+            </ul>
+        </div>
         <!-- 右下杯子 -->
         <img class="drink_icon" src="../assets/image/Entrance/drink.svg" alt="">
+
+         <!-- wave -->
+        <div>
+            <svg class="waves" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                </defs>
+                <g class="parallax">
+                    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(120, 190, 200,0.7" />
+                    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255, 193, 7,0.5)" />
+                    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(120, 190, 200,0.3)" />
+                    <use xlink:href="#gentle-wave" x="48" y="7" fill="rgb(120, 190, 200)" />
+                </g>
+            </svg>
+        </div>
     </main>
 </template>
 
