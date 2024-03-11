5f0e7916c5a61c46fae8cf996af5fb2140503fc3
XinLiao88 <ninexin8888@gmail.com>
2024-03-11 15:46:39 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',  // ※推上github用這個
+  base: '/thd104/g2/',  // ※推上github用這個
   // base: '', // ※本機測試php用這個 網址要+ /dist
   plugins: [
     vue(),
